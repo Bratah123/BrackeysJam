2a0cdd5e5133f2281caf8a9f670aac87c16ebc1b
Bratah123 <58405975+Bratah123@users.noreply.github.com>
2020-02-17 11:36:37 -0800

    Added a crude basis to the portal spawning

--- Brackey's Game Jam/Assets/Scripts/HoleSpawn.cs
+++ Brackey's Game Jam/Assets/Scripts/HoleSpawn.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HoleSummoner : MonoBehaviour
+public class HoleSpawn : MonoBehaviour
 {
-    public GameObject holes;
+    public GameObject portalPrefab;
+    public Transform HolePoint;
+
+    public int maxHoles = 1;
     public int holesAmount;
     void Start()
     {
@@ -14,6 +17,17 @@ public class HoleSummoner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject Portal = Instantiate(portalPrefab, HolePoint.transform.position, Quaternion.identity);
+
+            if (holesAmount >= maxHoles)
+            {
+                Destroy(Portal);
+                holesAmount--;
+            }
+                holesAmount++;
+        }
+
     }
 }
