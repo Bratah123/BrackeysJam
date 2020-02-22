152df87affd94d5a35455eea93828c86e24f8df1
Bratah123 <58405975+Bratah123@users.noreply.github.com>
2020-02-21 18:17:40 -0800

    Coded in Spikes, Kills you if you touch

---
+++ Brackey's Game Jam/Assets/SpikeHit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpikeHit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            PlayerController.deathAmount++;
+        }
+    }
+}
